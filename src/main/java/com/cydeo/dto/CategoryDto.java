1afca607519aee63f31766175bacd00cd2fb8f0a
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-27 17:49:33 -0600

    JDZH2-189  class CategoryDto  creation completed

--- src/main/java/com/cydeo/dto/CategoryDto.java
+++ src/main/java/com/cydeo/dto/CategoryDto.java
@@ -1,4 +1,24 @@
 package com.cydeo.dto;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
+
+    private Long id;
+
+    private String description;
+
+    @NotNull
+    private CompanyDto company;
+
+    private boolean hasProduct;
+
 }
