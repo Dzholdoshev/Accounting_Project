064d17045fa6144e8b1ac5370827b50dfd8f1db3
marklen <marklen86@gmail.com>
2022-12-27 20:10:11 -0600

    JDZH2_175_Create_ClientVendor / ClientVendor.java is created

--- src/main/java/com/cydeo/entity/ClientVendor.java
+++ src/main/java/com/cydeo/entity/ClientVendor.java
@@ -1,11 +1,34 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "clients_vendors")
+@Getter
+@Setter
+@NoArgsConstructor
 public class ClientVendor extends BaseEntity {
+  private String clientVendorName;
+  private String phone;
+  private String website;
+  @Enumerated(EnumType.STRING)
+  private ClientVendorType clientVendorType;
+  @OneToOne
+  @JoinColumn(name = "address_id")
+  private Address address;
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private Company company;
+
+
+
+
+
+
+
 }
