ddc76d1e95eddebe723e8b9f5cc7e2b5bfcc0942
Lorraine <lorrainecannom@gmail.com>
2022-12-28 15:56:39 -0500

    JDZH2-182 update

--- src/main/java/com/cydeo/converter/InvoiceDtoConverter.java
+++ src/main/java/com/cydeo/converter/InvoiceDtoConverter.java
@@ -1,25 +1,26 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.InvoiceDto;
+import lombok.SneakyThrows;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationPropertiesBinding
 public class InvoiceDtoConverter implements Converter<String, InvoiceDto> {
-  //  InvoiceService invoiceService;
+   private final InvoiceService invoiceService;
 
-   // public InvoiceDTOConverter(InvoiceService invoiceService) {
-  //      this.invoiceService = invoiceService;
-  //  }
+    public InvoiceDTOConverter(@Lazy InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
 
+    @SneakyThrows
     @Override
     public InvoiceDto convert(String id) {
-//        if (id == null || id.equals("")) {
-//            return null;
-//        }
-      // return invoiceService.findInvoiceById(Long.parseLong(id));
-        return null;
+
+        return invoiceService.findInvoiceById(Long.parseLong(id));
+
     }
 }
