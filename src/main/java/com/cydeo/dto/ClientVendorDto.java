f23214f63b69d7b7fd923d1991074092c661e2db
marklen <marklen86@gmail.com>
2022-12-27 22:40:10 -0600

    JDZH2_187_Create_ClientVendorDto / ClientVendorDto is created

--- src/main/java/com/cydeo/dto/ClientVendorDto.java
+++ src/main/java/com/cydeo/dto/ClientVendorDto.java
@@ -1,4 +1,29 @@
 package com.cydeo.dto;
 
+import com.cydeo.enums.ClientVendorType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientVendorDto {
+
+  private Long id;
+  private String clientVendorName;
+  private String phone;
+  private String website;
+  private ClientVendorType clientVendorType;
+  private AddressDto address;
+  private CompanyDto company;
+
+  public ClientVendorDto(String clientVendorName, String phone, String website, ClientVendorType clientVendorType, AddressDto address, CompanyDto company) {
+    this.clientVendorName = clientVendorName;
+    this.phone = phone;
+    this.website = website;
+    this.clientVendorType = clientVendorType;
+    this.address = address;
+    this.company = company;
+  }
 }
