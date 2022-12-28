9154615da096d5907e3d543af2435e40c6851f82
Lorraine <lorrainecannom@gmail.com>
2022-12-28 08:44:54 -0500

    JDZH2-218 list invoice and create invoice get methods

--- src/main/java/com/cydeo/controller/PurchaseInvoiceController.java
+++ src/main/java/com/cydeo/controller/PurchaseInvoiceController.java
@@ -1,11 +1,15 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.InvoiceDto;
 import com.cydeo.dto.InvoiceProductDto;
+import com.cydeo.enums.InvoiceType;
 import com.cydeo.service.InvoiceProductService;
 import com.cydeo.service.InvoiceService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class PurchaseInvoiceController {
     private final InvoiceService invoiceService;
@@ -24,6 +28,30 @@ public class PurchaseInvoiceController {
         //client vendor model.addAttribute("clientVendor", )
         //product model.addAttribute("
         return  "/invoice/purchase-invoice-update";
-
     }
+
+    @PostMapping("/update/{invoiceId}")
+    public String updateInvoice(@PathVariable("invoiceId")Long id, @ModelAttribute("invoiceDto") InvoiceDto invoiceDto, Model model) {
+        invoiceService.updateInvoice(invoiceDto);
+        return  "redirect: /PurchaseInvoices/list";
+    }
+
+    @GetMapping("/delete/{invoiceId}")
+    public String deleteInvoice(@PathVariable("invoiceId")Long id, Model model) {
+        invoiceService.deleteInvoice(id);
+        return  "redirect: /PurchaseInvoices/list";
+    }
+    @GetMapping("/list")
+    public String listInvoice(Model model){
+        model.addAttribute("Invoice", invoiceService.listAllInvoices(InvoiceType.PURCHASE));
+        return "/invoice/purchase-invoice-list";
+    }
+
+    @GetMapping("/create")
+    public String createInvoice(Model model){
+        model.addAttribute("invoices", invoiceService.createNewInvoiceDto());
+        //  model.addAttribute("clientVendor", )
+        return "/invoice/purchase-invoice-create";
+    }
+
 }
