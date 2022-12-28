c1bf73e184e75e2f3bc581e3cc23777336c4e5f2
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-12-28 15:51:22 -0500

    Product Entity created

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -1,9 +1,20 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ProductUnit;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "products")
 public class Product extends BaseEntity {
+
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+    private ProductUnit productUnit;
+    @ManyToOne
+    private Category category;
 }
