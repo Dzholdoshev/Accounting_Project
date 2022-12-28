d0941e97d652d4e9e89e1fa90ba6eb80a776ad4d
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-12-28 16:11:28 -0500

    Product Entity Lombok annotations added

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -2,11 +2,19 @@ package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
 import com.cydeo.enums.ProductUnit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "products")
 public class Product extends BaseEntity {
