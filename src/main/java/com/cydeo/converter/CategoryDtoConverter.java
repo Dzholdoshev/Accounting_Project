87571640249d0580563203a1e3b41101130973fd
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-27 19:49:29 -0600

    JDZH2-198 class CategoryDtoConverter creation completed

--- src/main/java/com/cydeo/converter/CategoryDtoConverter.java
+++ src/main/java/com/cydeo/converter/CategoryDtoConverter.java
@@ -1,4 +1,25 @@
 package com.cydeo.converter;
 
-public class CategoryDtoConverter {
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class CategoryDtoConverter implements Converter<String, CategoryDto> {
+
+  //  CategoryService categoryService;
+
+//    public CategoryService(CategoryService categoryService) {
+//        this.categoryService = categoryService;
+//    }
+    @Override
+    public CategoryDto convert(String source) {
+      //  if (source == null || source.equals("")){
+            return  null;
+        // }
+        // return categoryService.findById(Long.parseLong(source));;
+    }
 }
