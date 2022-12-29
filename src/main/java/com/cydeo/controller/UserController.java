9d2631e230be657b1ff15b7ddb7e88e6e148ac3f
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-29 11:02:08 -0600

    JDZH215- Basic CRUD User ServiceImpl created and crud methods implemented.

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,4 +1,84 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDto;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping("/list")
+    public String listAllUsers(Model model){
+
+        model. addAttribute("users", userService.listAllUsers());
+        return "user/user-list";
+    }
+
+
+    @GetMapping("/create")
+    public String createUser(Model model){
+
+        model.addAttribute("newUser", new UserDto());
+        model.addAttribute("userRoles", roleService.listAllRoles());
+        model.addAttribute("users", userService.listAllUsers());
+        return "user/user-create";
+    }
+
+    @PostMapping("/create")
+    public String saveUser(@ModelAttribute("user") UserDto user, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("users", userService.listAllUsers());
+            model.addAttribute("roles", roleService.listAllRoles());
+            return "user/user-create";
+        }
+        userService.save(user);
+        return "redirect:/user-create";
+    }
+
+
+    @GetMapping("/update/{username}")
+    public String editUser (@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user", userService.findByUsername(username));
+        model.addAttribute("roles", roleService.listAllRoles());
+        return "user/user-update";
+    }
+
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("user") UserDto userDto, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("users", userService.listAllUsers());
+            model.addAttribute("roles", roleService.listAllRoles());
+            return "user/user-update";
+        }
+        userService.update(userDto);
+        return "redirect:/user-list";
+    }
+
+
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteUser(username);
+        return "redirect:/user-list";
+    }
+
 }
