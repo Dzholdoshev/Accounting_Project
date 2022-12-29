77dd25d670f2f72d448f9122d4451d06c37035c8
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-29 11:03:28 -0600

    JDZH215- Basic CRUD User Service created and methods added

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -5,5 +5,14 @@ import com.cydeo.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+    List<UserDto> listAllUsers();
     UserDto findByUsername(String username);
+    void save(UserDto user);
+    //    void deleteByUserName(String username);
+    UserDto update(UserDto user);
+    void deleteUser(String username);
+    List<UserDto> listAllByRole(String role);
+
+    UserDto findUserById(Long id);
+
 }
