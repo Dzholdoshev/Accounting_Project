3257d060bb437d53e5ef4d7f5cf3ec74c4205c7d
Lorraine <lorrainecannom@gmail.com>
2022-12-29 12:49:43 -0500

    JDZH2-199 converter

--- src/main/java/com/cydeo/converter/InvoiceProductDtoConverter.java
+++ src/main/java/com/cydeo/converter/InvoiceProductDtoConverter.java
@@ -1,4 +1,22 @@
 package com.cydeo.converter;
 
-public class InvoiceProductDtoConverter {
+import com.cydeo.dto.InvoiceDto;
+import com.cydeo.service.InvoiceProductService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.convert.converter.Converter;
+
+public class InvoiceProductDtoConverter implements Converter<String, InvoiceDto> {
+    InvoiceProductService invoiceProductService;
+
+    public InvoiceProductDtoConverter(@Lazy InvoiceProductService invoiceProductService) {
+        this.invoiceProductService = invoiceProductService;
+    }
+
+    @Override
+    public InvoiceDto convert(String id) {
+        if (id == null || id.equals("")) {
+            return null;
+        }
+        return invoiceProductService.findInvoiceProductById(Long.parseLong(id));
+    }
 }
