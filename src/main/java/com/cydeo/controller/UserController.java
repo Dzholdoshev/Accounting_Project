c4ee8e135f24c10df697220be959281cfee3696f
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-30 11:14:07 -0600

    JDZH215- Basic CRUD UserController.

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -15,6 +15,8 @@ public class UserController {
     private final UserService userService;
     private final RoleService roleService;
 
+    //private final CompanyService companyService;
+
     public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
@@ -22,35 +24,11 @@ public class UserController {
 
 
     @GetMapping("/list")
-    public String listAllUsers(Model model){
+    public String listUsers(Model model) throws Exception {
 
-        model. addAttribute("users", userService.listAllUsers());
+        model. addAttribute("users", userService.getFilteredUsers());
         return "user/user-list";
     }
-
-
-    @GetMapping("/create")
-    public String createUser(Model model){
-
-        model.addAttribute("newUser", new UserDto());
-        model.addAttribute("userRoles", roleService.listAllRoles());
-        model.addAttribute("users", userService.listAllUsers());
-        return "user/user-create";
-    }
-
-    @PostMapping("/create")
-    public String saveUser(@ModelAttribute("user") UserDto user, BindingResult bindingResult, Model model){
-
-        if (bindingResult.hasErrors()){
-            model.addAttribute("users", userService.listAllUsers());
-            model.addAttribute("roles", roleService.listAllRoles());
-            return "user/user-create";
-        }
-        userService.save(user);
-        return "redirect:/user-create";
-    }
-
-
     @GetMapping("/update/{username}")
     public String editUser (@PathVariable("username") String username, Model model){
 
@@ -60,11 +38,38 @@ public class UserController {
     }
 
 
+    @PostMapping("/create")
+    public String createNewUser(@ModelAttribute("newUser")UserDto userDto,BindingResult result, Model model){
+        boolean emailExist = userService.emailExist(userDto); // write this method
+        if(result.hasErrors()||emailExist){
+            if (emailExist) {
+                result.rejectValue("username"," ","User already exists. Please try  with different username");
+            }
+            return "user/user-create";
+        }
+        userService.save(userDto);
+
+        return "redirect:/users/list";
+    }
+
+//    @PostMapping("/create")
+//    public String saveUser(@ModelAttribute("user") UserDto user, BindingResult bindingResult, Model model) throws Exception {
+//
+//        if (bindingResult.hasErrors()){
+//            model.addAttribute("users", userService.getFilteredUsers());
+//            model.addAttribute("roles", roleService.listAllRoles());
+//            return "user/user-create";
+//        }
+//        userService.save(user);
+//        return "redirect:/user-create";
+//    }
+
+
     @PostMapping("/update")
-    public String update(@ModelAttribute("user") UserDto userDto, BindingResult bindingResult, Model model){
+    public String update(@ModelAttribute("user") UserDto userDto, BindingResult bindingResult, Model model) throws Exception {
 
         if (bindingResult.hasErrors()){
-            model.addAttribute("users", userService.listAllUsers());
+            model.addAttribute("users", userService.getFilteredUsers());
             model.addAttribute("roles", roleService.listAllRoles());
             return "user/user-update";
         }
