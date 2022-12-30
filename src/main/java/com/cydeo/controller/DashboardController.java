0159906733558008b90b0bfbed9e5648a63abe39
ahmetilhan1 <ahmetilhanpolat@gmail.com>
2022-12-30 16:01:48 -0600

    JDZH2-Address Entity,DTO created

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -1,10 +1,8 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/dashboard")
 public class DashboardController {
 
 
