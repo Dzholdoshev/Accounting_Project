e370ffc58cf24c4446582ea3b8e9fdefd5f0d014
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-30 09:57:35 -0600

    JDZH215- Basic CRUD User Repository methods added

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -2,17 +2,27 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-   Optional<User>  findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    User findByUserNameAndIsDeleted(String username,Boolean deleted);
+    User findUserById(Long id);
 
-    List<User> findByRoleDescriptionIgnoreCaseAndIsDeleted(String description,Boolean deleted);
+    @Query("Select u from User u where u.isDeleted=?1 order by u.company.title, u.role.description ")
+    List<User> findAllUsersByCompanyAndRole(Boolean deleted);
+
+
+    User findByUserNameAndIsDeleted(Long id, boolean b);
+
+    User findByEmail(String email);
+
+
 }
