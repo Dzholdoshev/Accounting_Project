8922b3905015c395a4c4cd70393c2babb0dffdf4
ahmetilhan1 <ahmetilhanpolat@gmail.com>
2022-12-30 16:05:58 -0600

    JDZH2-Address Entity,DTO services are created.

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -9,17 +9,11 @@ import lombok.*;
 public class AddressDto {
 
     private Long id;
-
     private String addressLine1;
-
     private String addressLine2;
-
     private String city;
-
     private String state;
-
     private String country;
-
     private String zipCode;
 
 
