e60717455f8d42f66c71da79875ddf7bc6e5974b
ahmetilhan1 <ahmetilhanpolat@gmail.com>
2022-12-30 10:15:23 -0600

    Spring 1 including entity,DTO,Controller and Services

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -1,4 +1,25 @@
 package com.cydeo.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class AddressDto {
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String zipCode;
+
+
 }
