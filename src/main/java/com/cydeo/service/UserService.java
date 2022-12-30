997f523f723e95cbc58f4c3f6bbc0a718e05e841
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-30 09:57:08 -0600

    JDZH215- Basic CRUD User Service created and methods added

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,18 +1,20 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 public interface UserService {
-    List<UserDto> listAllUsers();
     UserDto findByUsername(String username);
-    void save(UserDto user);
-    //    void deleteByUserName(String username);
-    UserDto update(UserDto user);
-    void deleteUser(String username);
-    List<UserDto> listAllByRole(String role);
+
+    List<UserDto> getAllFilterForLoggedInUser(UserDto loggedInUser);
 
     UserDto findUserById(Long id);
 
+    List<UserDto> getFilteredUsers() throws Exception;
+    UserDto save(UserDto userDto);
+    UserDto update(UserDto userDto);
+    void delete(Long id);
+    Boolean emailExist(UserDto userDto);
 }
