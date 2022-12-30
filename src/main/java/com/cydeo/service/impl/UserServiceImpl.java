8e3e84e3d146763c117513fbf9cbe019a2fc5f26
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-30 09:58:28 -0600

    JDZH215- Basic CRUD UserServiceImpl methods implemented.

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -3,8 +3,8 @@ package com.cydeo.service.impl;
 import com.cydeo.dto.UserDto;
 import com.cydeo.entity.User;
 import com.cydeo.mapper.MapperUtil;
-import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -16,69 +16,119 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final SecurityService securityService;
     private final UserRepository userRepository;
     private final MapperUtil mapperUtil;
-    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil, UserMapper userMapper) {
+    public UserServiceImpl(SecurityService securityService, UserRepository userRepository,
+                           MapperUtil mapperUtil) {
+        this.securityService = securityService;
         this.userRepository = userRepository;
         this.mapperUtil = mapperUtil;
-        this.userMapper = userMapper;
-    }
-
-    @Override
-    public List<UserDto> listAllUsers() {
-        List<User> userList = userRepository.findAll();
-        return userList.stream().map(user -> mapperUtil.convert(user, new UserDto()))
-                .collect(Collectors.toList());
     }
 
     @Override
     public UserDto findByUsername(String username) {
-        return mapperUtil.convert(userRepository.findByUsername(username), new UserDto());
-    }
-
-    @Override
-    public void save(UserDto user) {
-        user.setPassWord(user.getPassWord());
-        user.setConfirmPassword(user.getPassWord());
-        User user1 = mapperUtil.convert(user, new User());
-        userRepository.save(user1);
-    }
-
-    public UserDto update(UserDto userDto) {
-        Optional<User> user = userRepository.findById(userDto.getId());
-        User convertedUser = mapperUtil.convert(user, new User());
-        convertedUser.setId(user.get().getId());
-        convertedUser.setPassWord(user.get().getPassWord());
-        userRepository.save(convertedUser);
-        return findUserById(user.getId());
-    }
-
-        @Override
-        public List<UserDto> listAllByRole (String role){
-            List<User> users = userRepository.findByRoleDescriptionIgnoreCaseAndIsDeleted(role, false);
-
-            return users.stream().map(userMapper::convertToDto).collect(Collectors.toList());
-        }
-
-    @Override
-    public UserDto findUserById(Long id) {
-        User user = userRepository.findById(id).orElseThrow(()->new NoSuchElementException("User not found"));
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new NoSuchElementException("User not found"));
         return mapperUtil.convert(user, new UserDto());
     }
 
-
     @Override
-        public void deleteUser (String username){
+    public List<UserDto> getAllFilterForLoggedInUser(UserDto loggedInUser) {
+        switch (loggedInUser.getRole().getDescription()) {
 
-            User user = userRepository.findByUsername(username).get();
+            case "Root User":
+                return getAllFilterForLoggedInUser(loggedInUser).stream()
+                        .filter(user -> user.getRole().getDescription().equals("Admin"))
+                        .collect(Collectors.toList());
 
-            user.setIsDeleted(true);
-            user.setUsername(user.getUsername() + "-" + user.getId());
-
-            userRepository.save(user);
+            case "Admin":
+                return getAllFilterForLoggedInUser(loggedInUser).stream()
+                        .filter(user -> user.getCompany().equals(loggedInUser.getCompany()))
+                        .collect(Collectors.toList());
+            default:
+                return findAllUsersByCompanyAndRole();
 
         }
     }
+
+    @Override
+    public UserDto findUserById(Long id) {
+        User user = userRepository.findUserById(id);
+        return mapperUtil.convert(user, new UserDto());
+    }
+
+    @Override
+    public List<UserDto> getFilteredUsers() throws Exception {
+        List<User> userList=userRepository.findAll();
+        return userList.stream().map(user -> mapperUtil
+                .convert(user,new UserDto())).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto save(UserDto userDto) {
+        userDto.setPassWord(userDto.getPassWord());
+        userDto.setConfirmPassword(userDto.getPassWord());
+        User user = mapperUtil.convert(userDto, new User());
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserDto update(UserDto userDto) {
+        Optional<User> user = userRepository.findById(userDto.getId());
+        User convertedUser = mapperUtil.convert(userDto, new User());
+        convertedUser.setId(user.get().getId());
+        convertedUser.setPassWord(user.get().getPassWord());
+        userRepository.save(convertedUser);
+        return findUserById(userDto.getId());
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        User user = userRepository.findById(id).get();
+        if (user==null){
+            throw new NoSuchElementException("User was not found");
+        }
+        user.setIsDeleted(true);
+        user.setUsername(user.getUsername()+"-"+user.getId());
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public Boolean emailExist(UserDto userDto) {
+         User user = userRepository.findByEmail(" ");
+        if (user != null) {
+            return true;
+        }
+        return false;
+    }
+
+    //    public List<UserDto> listAllUsersByLoggedInStatus() {
+//        if (securityService.getLoggedInUser().getRole().getDescription().equals("Admin")) {
+//            return listAllUsers().stream()
+//                    .filter(userDto -> userDto.getCompany().getId().equals(securityService.getLoggedInUser()
+//                            .getCompany().getId())).collect(Collectors.toList());
+//        } else if (securityService.getLoggedInUser().getRole().getDescription().equals("Root User")) {
+//            return listAllUsers().stream().filter(userDto -> userDto.getRole().getDescription().equals("Admin")).collect(Collectors.toList());
+//        } else {
+//            throw new NoSuchElementException("No users is available");
+//        }
+//    }
+       private List<UserDto> findAllUsersByCompanyAndRole() {
+
+          List<UserDto> list = userRepository.findAllUsersByCompanyAndRole(false).stream()
+            .map(currentUser -> {
+                Boolean isOnlyAdmin =
+                        currentUser.getRole().getDescription().equals("Admin");
+                UserDto userDto = mapperUtil.convert(currentUser, new UserDto());
+                userDto.setIsOnlyAdmin(isOnlyAdmin);
+                return userDto;
+            })
+            .collect(Collectors.toList());
+    return list;
+    }
+
+}
 
