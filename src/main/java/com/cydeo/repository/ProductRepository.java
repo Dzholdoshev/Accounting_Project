2244b14cbd506c8858f3d842c4252fe2cbcfc5a9
Abidullah Rahimi <abid_rahimi@hotmail.com>
2022-12-31 11:57:20 -0500

    Product Repository created

--- src/main/java/com/cydeo/repository/ProductRepository.java
+++ src/main/java/com/cydeo/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.cydeo.repository;
 
-import com.cydeo.dto.ProductDto;
+import com.cydeo.entity.Company;
 import com.cydeo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +10,13 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findAllByCategoryId(Long categoryId);
+    List<Product> findAllByCategoryCompany(Company company);
+
+    List<Product> findByCategoryId(Long categoryId);
 
     boolean existsByName(String name);
+
+    Product findByNameAndCategoryCompany(String name, Company actualCompany);
 
 
 }
