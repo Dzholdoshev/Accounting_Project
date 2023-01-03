8b283e8e233ee3a088981fabcefe4b62a6993e94
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-01-02 19:45:54 -0600

    JDZH2 - 228 UserDto validation added.

--- src/main/java/com/cydeo/dto/UserDto.java
+++ src/main/java/com/cydeo/dto/UserDto.java
@@ -1,10 +1,10 @@
 package com.cydeo.dto;
 import lombok.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
-@Getter
-@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
@@ -43,9 +43,91 @@ public class UserDto {
 
     Boolean isOnlyAdmin;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
+    public String getLastname() {
+        return lastname;
+    }
 
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        checkConfirmPassword();
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        checkConfirmPassword();
+    }
+    private void checkConfirmPassword() {
+        if (password != null && !password.equals(confirmPassword)) {
+            this.confirmPassword = null;
+        }
+    }
+
+    public RoleDto getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDto role) {
+        this.role = role;
+    }
+
+    public CompanyDto getCompany() {
+        return company;
+    }
+
+    public void setCompany(CompanyDto company) {
+        this.company = company;
+    }
+
+    public Boolean getIsOnlyAdmin() {
+        return isOnlyAdmin;
+    }
+
+    public void setIsOnlyAdmin(Boolean isOnlyAdmin) {
+        this.isOnlyAdmin = isOnlyAdmin;
+    }
 }
