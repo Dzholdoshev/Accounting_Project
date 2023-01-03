736bf90462fe3d09bd476a30ff0a30d4e67e5e1f
Abidullah Rahimi <abid_rahimi@hotmail.com>
2023-01-03 16:34:56 -0500

    Product controller navigateToProductCreate and createNewProduct methods are created

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProductDto;
+import com.cydeo.enums.ProductUnit;
 import com.cydeo.service.CategoryService;
 import com.cydeo.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/products")
@@ -19,15 +24,54 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/create")
-    public String creatProduct(Model model, Long categoryId){
+
+    @GetMapping("/list")
+    public String listAllProducts(Model model){
+        model.addAttribute("products", productService.getAllProducts());
+
+        return "product/product-list";
+    }
+
+    @GetMapping("/create")
+    public String navigateToProductCreate(Model model) throws Exception {
         model.addAttribute("newProduct", new ProductDto());
-        model.addAttribute("categories", categoryService.findCategoryById(categoryId));
 
-
-        return "/task/create";
+        return "product/product-create";
 
     }
 
+    @PostMapping("/create")
+    public String createNewProduct(@Valid @ModelAttribute("newProduct") ProductDto productDto, BindingResult bindingResult, Model model){
+
+        if (productService.isProductNameExist(productDto)){
+            bindingResult.rejectValue("name", " ", "product name already exist");
+        }
+
+        if (bindingResult.hasErrors()){
+            return "product/product-create";
+        }
+
+        productService.save(productDto);
+
+        return "redirect:/products/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateProduct(Model model, @PathVariable("{id}") Long id) throws Exception {
+
+        model.addAttribute("product", productService.findProductById(id));
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("productUnits", Arrays.asList(ProductUnit.values()));
+
+        return "redirect:/products/list";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@ModelAttribute("product") ProductDto productDto, @PathVariable("{id}") Long id){
+
+        productService.update(id, productDto);
+
+        return "redirect:/products/list";
+    }
 
 }
