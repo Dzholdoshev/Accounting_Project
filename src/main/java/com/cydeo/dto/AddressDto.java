e987c098735a8f7fadaf70f7a41cb022372fe9c9
ahmetilhan1 <ahmetilhanpolat@gmail.com>
2023-01-04 10:36:59 -0600

    JDZH2 - InvoiceServiceImpl LastThreeInvoices method updated

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -2,6 +2,9 @@ package com.cydeo.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,11 +12,15 @@ import lombok.*;
 public class AddressDto {
 
     private Long id;
+    @NotBlank(message = "Address can't be empty")
     private String addressLine1;
+    @NotBlank(message = "Address can't be empty")
     private String addressLine2;
     private String city;
     private String state;
     private String country;
+    @NotBlank(message = "Zip Code is required")
+    @Size(min = 5, max = 5)
     private String zipCode;
 
 
