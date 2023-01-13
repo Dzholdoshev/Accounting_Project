9e754c31a657b47f1efe9f0ffe928fe01f46617d
ahmetilhan1 <ahmetilhanpolat@gmail.com>
2023-01-13 09:59:36 -0600

    JDZH2 - ExchangeRatesTable created(api consumed) bug fixes

--- src/main/java/com/cydeo/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/DashboardServiceImpl.java
@@ -1,16 +1,11 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.client.CurrencyExchangeClient;
 import com.cydeo.dto.InvoiceDto;
-import com.cydeo.entity.Company;
-import com.cydeo.entity.User;
 import com.cydeo.enums.InvoiceStatus;
 import com.cydeo.enums.InvoiceType;
-import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.InvoiceRepository;
-import com.cydeo.service.CompanyService;
-import com.cydeo.service.DashboardService;
-import com.cydeo.service.InvoiceService;
-import com.cydeo.service.SecurityService;
+import com.cydeo.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -22,16 +17,14 @@ import java.util.Map;
 @Slf4j
 public class DashboardServiceImpl implements DashboardService {
 
-
-
     private final InvoiceService invoiceService;
     private final InvoiceRepository invoiceRepository;
     private final CompanyService companyService;
-    //private final CurrencyExchangeClient client;
+    private final CurrencyExchangeClient client;
 
-    public DashboardServiceImpl(InvoiceService invoiceService,/*, CurrencyExchangeClient client*/InvoiceServiceImpl invoiceServiceImpl, InvoiceRepository invoiceRepository, CompanyService companyService, MapperUtil mapperUtil, SecurityService securityService) {
+    public DashboardServiceImpl(InvoiceService invoiceService, CurrencyExchangeClient client, InvoiceRepository invoiceRepository, CompanyService companyService) {
         this.invoiceService = invoiceService;
-        //this.client = client;
+        this.client = client;
         this.invoiceRepository = invoiceRepository;
         this.companyService = companyService;
     }
@@ -53,16 +46,19 @@ public class DashboardServiceImpl implements DashboardService {
                 getSummaryNumbers.put("totalSales",totalSales);
                 getSummaryNumbers.put("profitLoss",new BigDecimal(100));  //for previewing
 
-                        /*invoiceService.getTotalPriceOfInvoice(
-                        invoiceRepository.findInvoicesByCompanyAndInvoiceStatusAndIsDeleted(
-                        new Company(), InvoiceStatus.APPROVED,false)))*/
         return getSummaryNumbers;
     }
 
 
-    //CurrencyDto getExchangeRates();
-
-
-
+//    public CurrencyDto getExchangeRates(){   //Tried Something
+//
+//        CurrencyDto currencyDto=new CurrencyDto();
+//        currencyDto.setEuro(client.getExchangeRates().stream().filter(CurrencyDto ->currencyDto.getEuro()).collect(Collectors.toList()));
+//        currencyDto.setBritishPound(client.getExchangeRates().getGbp());
+//        currencyDto.setCanadianDollar(client.getExchangeRates().getCad());
+//        currencyDto.setJapaneseYen(client.getExchangeRates().getJpy());
+//        currencyDto.setIndianRupee(client.getExchangeRates().getInr());
+//        return currencyDto;
+//    }
 
 }
